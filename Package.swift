30dab53b008b19a7c5d90e8facdef2aee7ea7baf
Pierluigi Cifani <pcifani@theleftbit.com>
2024-09-23 11:22:02 +0200

    First commit

--- Package.swift
+++ Package.swift
@@ -13,6 +13,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
         // 🐘 Fluent driver for Postgres.
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
+        
+        // 🐘 Fluent driver for SQLite.
+            .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.7.4"),
+        
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
     ],
@@ -22,6 +26,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
